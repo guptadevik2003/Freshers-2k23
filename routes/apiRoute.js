91dcd57288a92339ab398d8ddbd20bd5e7c6e50f
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2023-09-19 15:44:10 +0530

    Basic template ready

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -6,4 +6,9 @@ router.get('/', async (req, res) => {
     res.status(200).json({ success: true, message: `API Route Working!`, timestamp: Date.now() })
 })
 
+// /api/contact
+router.post('/contact', async (req, res) => {
+    res.json({ success: true, message: 'Working!', timestamp: Date.now() })
+})
+
 module.exports = router
